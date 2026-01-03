404d5dbe1bf10d58ce09f0e97c5c6c0c2fa51ede
KamatchiKarthi <karthivdj@gmail.com>
2026-01-03 14:40:13 +0530

    cors fix

--- Back_end/server.js
+++ Back_end/server.js
@@ -17,6 +17,9 @@ app.use(express.json());
 app.use(
   cors({
     origin: process.env.FRONT_END_URL,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
 
